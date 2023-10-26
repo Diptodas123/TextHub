2c4fe84c5d81f9342a4c12c0946f56556f81fba9
diptodas <dasdipto2004@gmail.com>
2023-10-27 04:41:30 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 export default function Navbar(props) {
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary mb-2 sticky-top" data-bs-theme={`${props.mode}`}  style={{boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px"}}>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary mb-2 sticky-top" data-bs-theme={`${props.mode}`} style={{ boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px" }}>
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/">{props.title}</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -19,7 +19,7 @@ export default function Navbar(props) {
                     </ul>
                     <div className="form-check form-switch">
                         <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                        <label className={`form-check-label text-${props.mode === "light" ? "dark" : "light"}`} htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
+                        <label className={`form-check-label text-${props.mode === "light" ? "dark" : "light"}`} htmlFor="flexSwitchCheckDefault">{(props.mode === "light" ? "Enable" : "Disable") + " Dark Mode"}</label>
                     </div>
                 </div>
             </div>
